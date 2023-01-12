9a50ed5ea3e69f4a8213d30238b049023fa99d91
denis <hugostiglitz590@gmail.com>
2023-01-12 17:03:29 +0300

    Create new lesson - Special User

---
+++ Lesson5_Special_user/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите ваше имя: ");
+string username = Console.ReadLine();
+
+if  (username.ToLower() == "виктор")
+{
+    Console.Write("Урааа! Пришёл Виктор! Привет, Виктор! Обажаааю тебя :*");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.Write(username);
+}
