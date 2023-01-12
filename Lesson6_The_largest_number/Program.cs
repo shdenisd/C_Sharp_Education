6d143c9681bdfbb58483f10a2c0ee75c7f48ba95
denis <hugostiglitz590@gmail.com>
2023-01-12 17:17:58 +0300

    Create new lesson - The largest number

---
+++ Lesson6_The_largest_number/Program.cs
@@ -0,0 +1,18 @@
+﻿int a = 1;
+int b = 3;
+int c = 32;
+int d = 200;
+int e = 13;
+int f = 5;
+
+int max = 0;
+
+if (a > max) max = a;
+if (b > max) max = b;
+if (c > max) max = c;
+if (d > max) max = d;
+if (e > max) max = e;
+if (f > max) max = f;
+
+Console.WriteLine(max);
+
