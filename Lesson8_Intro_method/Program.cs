5c6aca6e5b11249a591a68db47a0728be52f54d1
denis <hugostiglitz590@gmail.com>
2023-01-16 13:59:11 +0300

    New lesson about methods added

---
+++ Lesson8_Intro_method/Program.cs
@@ -0,0 +1,28 @@
+﻿int a1 = 100;
+int b1 = 12;
+int c1 = 10;
+int a2 = 159;
+int b2 = 2;
+int c2 = 48;
+int a3 = 0;
+int b3 = 7;
+int c3 = 3;
+
+
+int MostOfThree (int Number1, int Number2, int Number3)
+{
+    int max = Number1;
+    if (Number2 > max) max = Number2;
+    if (Number3 > max) max = Number3; 
+    return max;
+}
+
+
+int Max = MostOfThree
+(
+    MostOfThree(a1, b1, c1), 
+    MostOfThree(a2, b2, c2), 
+    MostOfThree(a3, b3, c3)
+);
+
+Console.WriteLine(Max);
